6df0c1d4a1cf642200a8df7a12f1004f48922c8e
Alexander Klementev <klementev@greensight.ru>
2023-02-28 02:19:46 +0300

    V0.11

--- src/components/MemoryGame/index.tsx
+++ src/components/MemoryGame/index.tsx
@@ -7,11 +7,22 @@ import Switch from "./components/Switch";
 import "./styles.scss";
 import { IGameMode } from "./types";
 
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 const cardsNumberOptions = [
   6, 8, 12, 16, 20, 24, 28, 30, 36, 40, 42, 48, 54, 56, 60, 70, 72, 80, 90, 100,
 ];
+
+const getClassesFromCardsNumber = (cardsNumber: number) => {
+  const columns = [3, 4, 5, 6, 7, 8, 9, 10];
+  return columns.reduce(
+    (acc, column) =>
+      cardsNumber % column === 0 && cardsNumber / column > 1
+        ? [...acc, `board${column}`]
+        : [...acc],
+    [] as string[]
+  );
+};
 
 const MemoryGame = () => {
   const [cardsNumber, setCardsNumber] = useState<number>(16);
@@ -31,84 +42,91 @@ const MemoryGame = () => {
     setIsNewGame(true);
   };
 
+  const boardClassNames = useMemo(
+    () => getClassesFromCardsNumber(cardsNumber),
+    [cardsNumber]
+  );
+
   return (
-    <div className="memory-game">
-      <div className={s.controls}>
-        <p>Move: {move}</p>
-        <div className={s.controlsButtons}>
-          <Button onClick={onNewGame} dark>
-            New game
-          </Button>
-          <Button onClick={resetGame} dark>
-            Reset
-          </Button>
-        </div>
-      </div>
-      <div
-        className={s.board}
-        style={{
-          // gridTemplateColumns: `repeat(auto-fit, minmax(75px, ${100 / 7}%))`,
-          gridTemplateColumns: `repeat(4, 1fr)`,
-        }}
-      >
-        <Overlay isVisible={isNewGame}>
-          <div className={s.newGroup}>
-            <p className={s.newMessage}>Choose field size:</p>
-            <ul className={s.newButtons}>
-              {cardsNumberOptions.map((option) => (
-                <li key={option}>
-                  <Button
-                    type="button"
-                    onClick={() => {
-                      onFieldSizeChoose(option);
-                    }}
-                  >
-                    {option}
-                  </Button>
-                </li>
-              ))}
-            </ul>
+    <div className={`memory-game-wrapper ${s.memoryGameWrapper}`}>
+      <div>
+        <div className={s.controls}>
+          <p>Move: {move}</p>
+          <div className={s.controlsButtons}>
+            <Button onClick={onNewGame} dark>
+              New game
+            </Button>
+            <Button onClick={resetGame} dark>
+              Reset
+            </Button>
           </div>
-          <div className={s.newGroup}>
-            <p className={s.newMessage}>Difficulty:</p>
-            <Switch
-              leftText="Normal"
-              rightText="Hardcore"
-              checked={gameMode === IGameMode.HARDCORE}
-              onChange={() => {
-                setGameMode((mode) => {
-                  return mode === IGameMode.NORMAL
-                    ? IGameMode.HARDCORE
-                    : IGameMode.NORMAL;
-                });
+        </div>
+        <div
+          className={`${s.board} ${boardClassNames
+            .map((boardClassName) => {
+              return s[boardClassName];
+            })
+            .join(" ")}`}
+        >
+          <Overlay isVisible={isNewGame}>
+            <div className={s.newGroup}>
+              <p className={s.newMessage}>Choose field size:</p>
+              <ul className={s.newButtons}>
+                {cardsNumberOptions.map((option) => (
+                  <li key={option}>
+                    <Button
+                      type="button"
+                      onClick={() => {
+                        onFieldSizeChoose(option);
+                      }}
+                    >
+                      {option}
+                    </Button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className={s.newGroup}>
+              <p className={s.newMessage}>Difficulty:</p>
+              <Switch
+                leftText="Normal"
+                rightText="Hardcore"
+                checked={gameMode === IGameMode.HARDCORE}
+                onChange={() => {
+                  setGameMode((mode) => {
+                    return mode === IGameMode.NORMAL
+                      ? IGameMode.HARDCORE
+                      : IGameMode.NORMAL;
+                  });
+                }}
+              />
+              <p className={s.newDifficultyMessage}>
+                {gameMode === IGameMode.HARDCORE
+                  ? "*Guess all cards in one go"
+                  : "*Guessed card pairs stay uncovered"}
+              </p>
+            </div>
+          </Overlay>
+          <Overlay isVisible={isGameWon}>
+            <p className={s.wonMessage}>You won in {move} moves!</p>
+            <div className={s.wonButtons}>
+              <Button onClick={onNewGame}>New game</Button>
+              <Button onClick={resetGame}>Reset</Button>
+            </div>
+          </Overlay>
+          {cards.map(({ emoji, isGuessed, isPicked }, index) => (
+            <Card
+              key={`${emoji}-${index}`}
+              emoji={emoji}
+              isGuessed={isGuessed}
+              isPicked={isPicked}
+              isDisabled={isDisabled || isNewGame}
+              onPickCard={() => {
+                onPickCard(index);
               }}
             />
-            <p className={s.newDifficultyMessage}>
-              {gameMode === IGameMode.HARDCORE
-                ? "*Guess all cards in one go"
-                : "*Guessed card pairs stay uncovered"}
-            </p>
-          </div>
-        </Overlay>
-        <Overlay isVisible={isGameWon}>
-          <p className={s.wonMessage}>You won in {move} moves!</p>
-          <div className={s.wonButtons}>
-            <Button onClick={onNewGame}>New game</Button>
-            <Button onClick={resetGame}>Reset</Button>
-          </div>
-        </Overlay>
-        {cards.map(({ emoji, isGuessed, isPicked }, index) => (
-          <Card
-            key={`${emoji}-${index}`}
-            emoji={emoji}
-            isGuessed={isGuessed}
-            isPicked={isPicked}
-            isDisabled={isDisabled || isNewGame}
-            onPickCard={() => {
-              onPickCard(index);
-            }}
-          />
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
